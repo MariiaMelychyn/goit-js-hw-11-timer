f7fcb922479ac12deec4917f176de659aed82887
MariiaMelychyn <lv080191mmi75@gmail.com>
2021-08-04 22:32:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,3 +18,21 @@ class CountdownTimer {
         stopText(timerDate);
     },INTERVAL_DELAY);
 }
+
+function pad(value){
+    return String(value).padStart(2, "0");
+}
+function changeTime(time){
+    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    refs.days.textContent = `$(days)`;
+    refs.hours.textContent = `$(hours)`;
+    refs.mins.textContent = `$(mins)`;
+    refs.secs.textContent = `$(secs)`;
+}
+
+
+
